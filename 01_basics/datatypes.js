92499c7c7fac1226cd9242d8544d4b3c0977af4f
Shadowsvg <63726448+Shadowsvg@users.noreply.github.com>
2023-01-23 17:50:36 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Shadow"
+let number = 3
+let isBoolean = true
+let city;
+
+/*
+number => 2 to the power 52
+bigint
+string => ""
+boolean => true/false
+null => standlone value
+undefined => value is not assigned
+symbol => unique
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
