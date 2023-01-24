431cce42b70c6c5dc9e8adb3b02dd781ade94c5a
Shadowsvg <63726448+Shadowsvg@users.noreply.github.com>
2023-01-24 08:37:39 +0000

    conversion operations 2

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -6,7 +6,7 @@ let score = 89
 
 let str = "24ef"
 let num = Number(str)
-// console.log(num)     // output 'NaN' because string cannot converted to number
+// console.log(num)     // output 'NaN' because string cannot be converted to number
 
 let isGood = true
 let a = Number(isGood)
@@ -14,4 +14,21 @@ let a = Number(isGood)
 
 // "" => false
 // "shadow" => true
+
+/*----------------Conversion Operations--------------------*/
+
+console.log("1" + 2 + 4) // output => 124
+console.log(1 + 2 + "4") // output => 34
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// output: "x:4, y:3"
+
+let c = 3;
+const b = ++c;
+
+console.log(`a:${c}, b:${b}`);
+// output: "a:4, b:4"
 
