952cfd7bc1eadeecf1ddbe4a5e1038730a78d548
Shadowsvg <63726448+Shadowsvg@users.noreply.github.com>
2023-01-24 08:01:46 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,17 @@
+let score = 89
+// console.log(typeof(score))        // number
+
+// console.log(typeof(NaN))          // number
+// console.log(typeof(undefined))    // undefined
+
+let str = "24ef"
+let num = Number(str)
+// console.log(num)     // output 'NaN' because string cannot converted to number
+
+let isGood = true
+let a = Number(isGood)
+// console.log(a);      // ture => 1; false => 0
+
+// "" => false
+// "shadow" => true
+
